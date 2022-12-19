d5ed7b71b35b0da889ce6edd5bd1ec2fcca462ee
Gulab Singh SIkarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2022-12-19 19:55:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './Components/Login/Login';
+import { Routes, Route, useNavigate} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import HomePage from './Components/pages/pageLayout/homePage';
+import SpecificPage from './Components/pages/SpecificMoviePage/SpecificPage';
+import Navbar from './Components/Utility/Navbar/Navbar';
 function App() {
+  const [user, setUser] = useState();
+  const  [isUser , setIsUser] =useState(false);
+
+  const updateUser = () => {
+
+    const demo_user = {
+      name: "test_user",
+      password: "password"
+    }
+    setIsUser(true)
+
+    setUser(demo_user)
+
+  }
+  const navigate =useNavigate()
+
+  useEffect(() => {
+    if (!user)  {  navigate('/login') }
+
+  }, [])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+{
+  (isUser)?<Navbar/>:<></>
+}
+
+      <Routes>
+
+        <Route path='/movies/:pageNumber' element={<HomePage />} />
+        <Route path='/login' element={<Login updateUser={updateUser} />} />
+        <Route path='/movies/movie/:movieId' element={<SpecificPage />} />
+
+
+
+
+      </Routes>
     </div>
   );
 }
